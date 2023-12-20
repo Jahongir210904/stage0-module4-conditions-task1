e6a0b49348d8ca527d2ae5101267b89c123bc627
Jahongir04 <jahongirachilov004@gmail.com>
2023-12-20 12:25:01 +0500

    fix condition false

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -3,9 +3,9 @@ package school.mjc.stage0.conditions.task1;
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
         if (false){
-            System.out.println("false");
-        }else {
 
+        }else {
+            System.out.println("false");
         }
     }
 }
